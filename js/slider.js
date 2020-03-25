b210b3316cbda95dd2909e87ebd2f09432cfd5fb
AlexHlukhyi <AlexHlukhyi@users.noreply.github.com>
2020-03-26 01:32:16 +0200

    v0.2

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    let items = document.getElementsByClassName('slider-item');
+    let indicators = document.getElementsByClassName('indicator');
+
+    for (let i = 0; i < items.length; i++) {
+
+    }
+};
