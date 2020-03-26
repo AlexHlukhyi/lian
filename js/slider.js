5211bb735379f8b7af15ebdc366201e4e77478b0
AlexHlukhyi <AlexHlukhyi@users.noreply.github.com>
2020-03-27 01:20:06 +0200

    v0.3 home page done
    - need to fix css structure
    - need make page adaptive
    - need to change images to fontawesome icons

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,38 @@
 window.onload = function () {
-    let items = document.getElementsByClassName('slider-item');
+    let index = 0;
+    // slides init
+    let slides = document.getElementsByClassName('slider-item');
+    let slideInterval = setInterval(nextSlide,5000);
+    // indicators init
+    for (let i = 0; i < slides.length; i++) {
+        let indicator = document.createElement('div');
+        indicator.className = 'indicator';
+        indicator.onclick = function() {
+            goToSlide(i);
+        };
+        document.getElementById('indicators').appendChild(indicator);
+    }
     let indicators = document.getElementsByClassName('indicator');
+    indicators[index].classList.toggle('active');
+    // controls init
+    document.getElementById('button-next').onclick = nextSlide;
+    document.getElementById('button-previous').onclick = previousSlide;
 
-    for (let i = 0; i < items.length; i++) {
+    function nextSlide() {
+        goToSlide(index + 1);
+    }
 
+    function previousSlide() {
+        goToSlide(index - 1);
+    }
+
+    function goToSlide(n) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide,5000);
+        slides[index].classList.toggle('visible');
+        indicators[index].classList.toggle('active');
+        index = (n + slides.length) % slides.length;
+        slides[index].classList.toggle('visible');
+        indicators[index].classList.toggle('active');
     }
 };
